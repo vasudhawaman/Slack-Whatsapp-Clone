3ab6b94a79493207c54338076091c3efb142c1cf
vasudhawaman <vasudhawaman@gmail.com>
2024-10-02 23:20:59 +0530

    OAUTH callback route

--- server/OAuth/googleOauth.js
+++ server/OAuth/googleOauth.js
@@ -10,7 +10,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "krishkrish@123"; // Make sure your
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'http://localhost:8000/auth/google/callback',
+    callbackURL: 'https://talkpal-backend.onrender.com/auth/google/callback',
     passReqToCallback: true
 },
 async (request, accessToken, refreshToken, profile, done) => {
