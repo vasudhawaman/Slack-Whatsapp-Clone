0a1ca5e13d247b321fa9c702d3a460e90008960c
vasudhawaman <vasudhawaman@gmail.com>
2024-10-03 11:21:57 +0530

    cors fixed again

--- server/index.js
+++ server/index.js
@@ -39,6 +39,7 @@ app.use(passport.session());
 app.use(cookieParser());
 app.use(cors({
     origin: 'https://talk-pal-alpha.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true
 }));
 
